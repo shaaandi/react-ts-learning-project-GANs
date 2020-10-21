0b74cd9613a7127599569b2b6aaddcfbad3c2719
shaaandi <saad.edtech@gmail.com>
2020-10-21 17:17:47 +0500

    implement initial setup of upload component

--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -1,25 +1,150 @@
-import React from 'react';
-import { Upload } from 'antd';
+import React, { useState, useRef, useEffect } from 'react';
+import styled from 'styled-components';
+import { Upload, Row, Col, Button, Spin } from 'antd';
+import ImgCrop from 'antd-img-crop';
 import { InboxOutlined } from '@ant-design/icons';
 import { iWhatIfContext } from '../screens/WhatIf';
+import { RcFile } from 'antd/lib/upload';
 
 interface iProps {
     context: iWhatIfContext;
 }
 
-const Uploader: React.FC<iProps> = (props) => {
+interface iPreviewImg {
+    show: boolean;
+}
+
+const PreviewImg = styled.image<iPreviewImg>`
+    display: ${({ show }) => (show ? 'block' : 'none')};
+`;
+
+var same = function (str: string, callback: (a: string) => void) {
+    setTimeout(function () {
+        callback(str);
+    }, 3000);
+};
+
+let fakeFetch = () => {
+    return new Promise((res, rej) => {
+        same('full', (s: string) => res(s));
+    });
+};
+
+const Uploader: React.FC<iProps> = ({ context }) => {
+    const { uploadFile } = context;
+    const [loading, setLoading] = useState<boolean>(false);
+    const previewRef = useRef(null);
+
+    // const onPreview = async (file: any) => {
+    // let src = file.url;
+    // if (!src) {
+    //     src = await new Promise((resolve) => {
+    //         const reader = new FileReader();
+    //         reader.readAsDataURL(file.originFileObj);
+    //         reader.onload = () => resolve(reader.result);
+    //     });
+    // }
+    //     const image = new Image();
+    //     image.src = src;
+    //     const imgWindow = window.open(src)!;
+    //     imgWindow.document.write(image.outerHTML);
+    // };
+
+    useEffect(() => {
+        if (uploadFile) {
+            const image = document.getElementById('output');
+            (image as HTMLImageElement).src = URL.createObjectURL(uploadFile);
+        }
+    }, [uploadFile]);
+
+    const handleUpload = async (choice: string) => {
+        // const formData = new FormData();
+        // formData.append('file', uploadFile);
+        // formData.append('choice', choice);
+        setLoading(true);
+        fakeFetch().then(() => {
+            // here we can fake the request;
+            // we retuned with a url of the result
+            setLoading(false);
+            context.updateContext((p) => ({
+                ...p,
+                choice,
+                preview:
+                    'https://cdn.pixabay.com/photo/2017/10/31/07/49/horses-2904536_960_720.jpg',
+            }));
+        });
+    };
+
+    const handleLoadImage = (file: RcFile) => {
+        context.updateContext((p) => ({ ...p, uploadFile: file }));
+        return false;
+    };
+
+    const handleRemove = () => {
+        context.updateContext((p) => ({ ...p, uploadFile: false }));
+    };
+
+    const submitButtons = [
+        {
+            title: 'Male',
+            id: 'male',
+            layout: { xs: 8 },
+            onClick: () => handleUpload('male'),
+        },
+        {
+            title: 'Female',
+            id: 'female',
+            layout: { xs: 8 },
+            onClick: () => handleUpload('female'),
+        },
+        {
+            title: 'Old',
+            id: 'old',
+            layout: { xs: 8 },
+            onClick: () => handleUpload('old'),
+        },
+    ];
+
     return (
-        <Upload.Dragger name="files" beforeUpload={(e) => false}>
-            <p className="ant-upload-drag-icon">
-                <InboxOutlined />
-            </p>
-            <p className="ant-upload-text">
-                Click or drag file to this area to upload
-            </p>
-            <p className="ant-upload-hint">
-                Support for a single or bulk upload.
-            </p>
-        </Upload.Dragger>
+        <Row>
+            <Col xs={24} style={{ marginBottom: '20px' }}>
+                <Row justify="center">
+                    {uploadFile && (
+                        <img id="output" width="60%" alt="uploaded preview" />
+                    )}
+                    {true && (
+                        <div style={{ width: '60%' }}>
+                            <ImgCrop rotate>
+                                <Upload
+                                    fileList={
+                                        uploadFile ? [uploadFile as RcFile] : []
+                                    }
+                                    beforeUpload={handleLoadImage}
+                                    onRemove={handleRemove}
+                                >
+                                    {!uploadFile && '+ Upload'}
+                                </Upload>
+                            </ImgCrop>
+                        </div>
+                    )}
+                </Row>
+            </Col>
+            {submitButtons.map(({ title, id, onClick, layout }) => (
+                <Col {...layout} key={id}>
+                    <Button
+                        type="primary"
+                        onClick={onClick}
+                        disabled={!uploadFile}
+                        style={{
+                            margin: '15px 0px',
+                        }}
+                        block
+                    >
+                        {title}
+                    </Button>
+                </Col>
+            ))}
+        </Row>
     );
 };
 
--- src/screens/WhatIf.tsx
+++ src/screens/WhatIf.tsx
@@ -1,52 +1,55 @@
-import React, {useState} from 'react';
-import {Row, Col} from "antd";
-import {Header, Uploader} from "../components"
+import React, { useState } from 'react';
+import { Row, Col } from 'antd';
+import { Header, Uploader } from '../components';
+import { RcFile } from 'antd/lib/upload';
 
 export interface iWhatIfContextState {
     preview: string;
     choice: string;
+    uploadFile: boolean | RcFile;
 }
 
 export interface iWhatIfContext extends iWhatIfContextState {
-    updateContext: React.Dispatch<React.SetStateAction<iWhatIfContextState>>
+    updateContext: React.Dispatch<React.SetStateAction<iWhatIfContextState>>;
 }
-
 
 const initialContextState = {
-    preview : '',
-    choice: ""
-}
+    preview: '',
+    choice: '',
+    uploadFile: false,
+};
 
 const WhatIf = () => {
+    const [state, setState] = useState<iWhatIfContextState>(
+        initialContextState
+    );
 
-    const [state, setState] = useState<iWhatIfContextState>(initialContextState);
-
-    const Context:React.Context<iWhatIfContext> = React.createContext({...state, updateContext:setState});
+    const Context: React.Context<iWhatIfContext> = React.createContext({
+        ...state,
+        updateContext: setState,
+    });
 
     return (
-        <Context.Provider value={{...state, updateContext:setState}}>
-            <Row style={{width: "100%"}} justify="center">
+        <Context.Provider value={{ ...state, updateContext: setState }}>
+            <Row style={{ width: '100%' }} justify="center">
                 <Col xs={24} lg={24} xl={20} xxl={15}>
                     <Row>
                         <Col xs={24}>
-                          <Context.Consumer>
-                              {value => <Header context={value} />}
-                          </Context.Consumer>
+                            <Context.Consumer>
+                                {(value) => <Header context={value} />}
+                            </Context.Consumer>
                         </Col>
                         <Col xs={12}>
-                             <Context.Consumer>
-                              {value => <Uploader context={value} />}
-                          </Context.Consumer>
+                            <Context.Consumer>
+                                {(value) => <Uploader context={value} />}
+                            </Context.Consumer>
                         </Col>
-                        <Col xs={12}>
-                            {/* here will be our preview */}
-                        </Col>
+                        <Col xs={12}>{/* here will be our preview */}</Col>
                     </Row>
                 </Col>
             </Row>
         </Context.Provider>
-    )
+    );
 };
-
 
 export default WhatIf;
